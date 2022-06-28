34d5da978dcc2801f453d9973e91ce33fd27cdbe
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2022-06-28 15:35:00 +0300

    hardhat refractors

--- apps/hardhat/.eslintrc.js
+++ apps/hardhat/.eslintrc.js
@@ -1,24 +1,23 @@
 module.exports = {
   env: {
-    browser: false,
-    es2021: true,
     mocha: true,
-    node: true,
   },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 12,
-  },
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  plugins: ["babel"],
   rules: {
-    "node/no-unsupported-features/es-syntax": [
+    "prettier/prettier": ["error"],
+    "import/extensions": [
       "error",
-      { ignores: ["modules"] },
+      "ignorePackages",
+      {
+        js: "never",
+        ts: "never",
+      },
     ],
+    "import/prefer-default-export": "off",
+    "prefer-destructuring": "off",
+    "prefer-template": "off",
+    "no-console": "off",
+    "func-names": "off",
   },
 };
